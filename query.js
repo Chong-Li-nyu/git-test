47f20600cbb84df846ac41055c2c4350a289c240
Chong-Li-nyu <cl4056@nyu.edu>
2019-01-22 14:16:09 -0500

    changed need to be overrid when merging

--- query.js
+++ query.js
@@ -1,4 +1,11 @@
-Start edditing for master
+This is change from master, and it will be overrid by the hotfix Start edditing for master
 
+
+newly added lines here 
+---
+----
+
+-----
+ends of newly added.
 
 

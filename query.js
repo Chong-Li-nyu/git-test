bce9053564c2666981f5d7ec7526150ed9392881
Chong-Li-nyu <cl4056@nyu.edu>
2019-01-24 14:48:19 -0500

    Revert "commit diverge with origin master"
    This reverts commit f4890d759db738b184e4d83a54f91cc0501a90f9.

--- query.js
+++ query.js
@@ -1,4 +1,5 @@
-This is change from master, and it will be overrid by the hotfix Start edditing for master. change after origin/master, and need to be rebase on origin/master
+This is change from master, and it will be overrid by the hotfix Start edditing for master
+
 
 newly added lines here 
 ---
